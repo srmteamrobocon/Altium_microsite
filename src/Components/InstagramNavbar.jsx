501f0598fb5e28bafcd40592eb7d3029b7d2955f
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2023-09-21 18:35:27 +0530

    navbar fixed by Abhinav

--- src/Components/InstagramNavbar.jsx
+++ src/Components/InstagramNavbar.jsx
@@ -3,6 +3,7 @@ import Homeimg from '../assets/home_icon.svg';
 import applyimg from '../assets/apply_icon.png';
 import aboutimg from '../assets/about_icon.svg';
 import contactimg from '../assets/contact_icon.svg';
+import Hero from './Hero';
 
 const InstagramNavbar = () => {
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768); // Set the threshold width for mobile view
@@ -30,57 +31,63 @@ const InstagramNavbar = () => {
   if (isMobile) {
     // Render the mobile navigation bar
     return (
-      <div className="relative">
-        <nav className="fixed bottom-0 w-full bg-opacity-70 backdrop-blur-sm z-10">
-          <div className="flex justify-between mx-4 p-2">
-            <a href="#" className="p-1">
-              <img src={Homeimg} alt="Home" className="w-6 h-6 dark:invert" />
-            </a>
-            <a href="#" className="p-1">
-              <img src={aboutimg} alt="About" className="w-6 h-6 dark:invert" />
-            </a>
-            <a href="#apply" className="p-1">
-              <img src={applyimg} alt="Apply" className="w-6 h-6 dark:invert" />
-            </a>
-            <a href="#" className="p-1">
-              <img src={contactimg} alt="Apply" className="w-6 h-6 dark:invert" />
-            </a>
-          </div>
-        </nav>
-      </div>
+      <>
+        <Hero />
+        <div className="relative">
+          <nav className="fixed bottom-0 w-full bg-opacity-70 backdrop-blur-sm z-10">
+            <div className="flex justify-between mx-4 p-2">
+              <a href="#" className="p-1">
+                <img src={Homeimg} alt="Home" className="w-6 h-6 dark:invert" />
+              </a>
+              <a href="#" className="p-1">
+                <img src={aboutimg} alt="About" className="w-6 h-6 dark:invert" />
+              </a>
+              <a href="#apply" className="p-1">
+                <img src={applyimg} alt="Apply" className="w-6 h-6 dark:invert" />
+              </a>
+              <a href="#" className="p-1">
+                <img src={contactimg} alt="Apply" className="w-6 h-6 dark:invert" />
+              </a>
+            </div>
+          </nav>
+        </div>
+      </>
     );
   } else {
     // Render the desktop navigation bar
     return (
-      <nav className="bg-blue-gray-900 bg-opacity-70 backdrop-blur-sm text-black dark:text-white py-4 sticky top-0 z-10">
-        <div className="container mx-auto flex justify-between items-center">
-          <a href="#" className="text-2xl font-bold">
-            SRM Team Robocon
-          </a>
-          <ul className="flex space-x-6">
-            <li>
-              <a href="#" className="hover:text-blue-500">
-                Home
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-blue-500">
-                About
-              </a>
-            </li>
-            <li>
-              <a href="#apply" className="hover:text-blue-500">
-                Apply
-              </a>
-            </li>
-            <li>
-              <a href="#" className="hover:text-blue-500">
-                Contact
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <>
+        <Hero />
+        <nav className="fixed bg-blue-gray-900 bg-opacity-70 backdrop-blur-sm text-black dark:text-white py-4 top-0 z-10 w-screen">
+          <div className="container mx-auto flex justify-between items-center">
+            <a href="#" className="text-2xl font-bold">
+              SRM Team Robocon
+            </a>
+            <ul className="flex space-x-6">
+              <li>
+                <a href="#" className="hover:text-blue-500">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-blue-500">
+                  About
+                </a>
+              </li>
+              <li>
+                <a href="#apply" className="hover:text-blue-500">
+                  Apply
+                </a>
+              </li>
+              <li>
+                <a href="#" className="hover:text-blue-500">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </>
     );
   }
 };
