30accbb3b5d2af1d723644677dbfb5f783fd90d4
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2023-09-24 01:10:48 +0530

    Changes footer title to SPACED 😃🥺

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -59,7 +59,11 @@ const Footer = () => {
                     <div className="row p-4 pb-7 md:pb-0">
                         <div className="">
                             <hr />
-                            <p className="flex justify-center text-center mt-2">Innovation in Every Byte,<br /> © 2023 SRM Team Robocon</p>
+                            <p className="flex justify-center text-center mt-2">
+                                Innovation in Every Byte,
+                                <br />
+                                © 2023 SPACED
+                            </p>
                         </div>
                     </div>
                 </div>
