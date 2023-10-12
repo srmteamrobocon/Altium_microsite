bb55b113ba8b45a12e78384f9af2c915f34f0cd6
Diptayan Jash <111650788+djdiptayan1@users.noreply.github.com>
2023-10-13 01:07:02 +0530

    updated styling

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -20,16 +20,16 @@ const About = () => {
                 </div> */}
 
                 <div className="flex justify-center items-center mb-5">
-                    <div className="card text-gray-300 w-[clamp(260px,80%,300px)] hover:brightness-90 transition-all cursor-pointer group bg-gradient-to-tl from-gray-900 to-gray-950 hover:from-gray-800 hover:to-gray-950 border-r-2 border-t-2 border-gray-900 m-4 rounded-lg overflow-hidden relative">
+                    <div className="card text-gray-200 w-[clamp(260px,80%,300px)] hover:brightness-90 transition-all cursor-pointer group bg-gradient-to-tl from-gray-900 to-gray-950 hover:from-gray-800 hover:to-gray-950 border-r-2 border-t-2 border-gray-900 m-4 rounded-lg overflow-hidden relative">
                         <div className="px-8 py-10">
                             <div className="bg-orange-500 w-10 h-10 rounded-full rounded-tl-none mb-4 group-hover:-translate-y-1 group-hover:shadow-xl group-hover:shadow-red-900 transition-all"></div>
                             <div className="uppercase font-bold text-xl">
                                 9:00 am
                             </div>
-                            <div className="text-gray-300 uppercase tracking-widest">
+                            <div className="text-gray-200 uppercase tracking-widest">
                                 October 17th-18th 2023
                             </div>
-                            <div className="text-gray-400 mt-8">
+                            <div className="text-gray-200 mt-8">
                                 <a href="https://maps.app.goo.gl/ktk7RmfYyCDRR462A" target="_blank"><p className="font-bold">Digital Image Processing Lab,</p>
                                     <p>Hi-Tech Building, Main Campus</p></a>
                             </div>
@@ -41,13 +41,13 @@ const About = () => {
 
                 <div className="grid grid-cols-1 md:grid-cols-2 gap-10 pl-2 pr-2">
                     <div className="md:text-left">
-                        <h3 className="text-xl font-Orbitron font-semibold mb-4 underline">Why Altium Designer ?</h3>
+                        <h3 className="text-2xl font-Orbitron font-semibold mb-4 underline">Why Altium Designer ?</h3>
                         <p className=" text-gray-300 font-Poppins">
                             As you delve into Altium Designer, you'll discover its prowess in catering to engineers globally. Its extensive feature set covers schematic capture, PCB layout, simulation, and manufacturing capabilities, while its potent design engine effortlessly handles complex projects thus enabling you to turn your creative ideas into functional, manufacturable PCBs.
                         </p>
                     </div>
                     <div className="md:text-left ">
-                        <h3 className="text-xl font-Orbitron font-semibold mb-4 underline">What You Will Learn</h3>
+                        <h3 className="text-2xl font-Orbitron font-semibold mb-4 underline">What You Will Learn</h3>
                         <ul className="list-disc text-gray-300 font-Poppins">
                             <li>Mastering Altium Designer</li>
                             <li>Schematic Design</li>
@@ -65,10 +65,10 @@ const About = () => {
                     With limited availability, it is imperative to secure your place today. Seize this opportunity to embark on your journey towards proficiency in PCB design, powered by Altium Designer, a distinguished industry-standard software for <span className="text-xl font-bold">Rs 549</span>  only. Your path to becoming a proficient PCB designer begins here. <br /><span className=" font-Poppins font-bold text-yellow-200">"The enrolled participants will receive communication regarding the subsequent steps at a later time."</span>
                 </p>
                 {/* Speaker Section */}
-                <h2 className="text-3xl font-Orbitron font-semibold mb-4 p-4">Meet Our Speaker</h2>
+                <h2 className="text-3xl font-Orbitron font-semibold mb-1 mt-3 p-4">Meet Our Speaker</h2>
 
             </div>
-            <div className="flex justify-center items-center mt-2">
+            <div className="flex justify-center items-center">
                 <div className="relative group duration-500 cursor-pointer group overflow-hidden text-black w-56 rounded-2xl hover:duration-700 shadow-lg">
                     <img
                         src={Img} // Replace with the URL of your image
